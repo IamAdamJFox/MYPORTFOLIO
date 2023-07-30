66f6da52f3f54b0e0df1f4b0309e3df7989a8c5d
adamjfox <adamjeffersonfox@gmail.com>
2023-07-29 20:37:42 -0700

    app and index

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import About from './components/About';
+import Portfolio from './components/Portfolio';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+import Footer from './components/Footer';
+
+import "./Style/App.css";
+
+function App() {
+    return (
+      <div className="App">
+        <Router>
+        <Header/>
+          <div className="container mt-5">
+            <Routes>
+              <Route path="/MYPORTFOLIO/" element={<About />} />
+           
+              <Route path="/MYPORTFOLIO/about" element={<About />} />
+              <Route path="/MYPORTFOLIO/portfolio" element={<Portfolio />} />
+              <Route path="/MYPORTFOLIO/contact" element={<Contact />} />
+              <Route path="/MYPORTFOLIO/resume" element={<Resume />} />
+            </Routes>
+          </div>
+          <Footer />
+        </Router>
+      </div>
+    );
+  }
+
+  export default App;
