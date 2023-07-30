4ba5117ae365fb64d8e9f597d58e905472de9985
adamjfox <adamjeffersonfox@gmail.com>
2023-07-29 20:30:14 -0700

    Projects

---
+++ src/components/Project.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "../Style/Project.css";
+
+const Project = ({ title, description, imgSrc, imgAlt, deployedLink, githubLink }) => {
+  return (
+    <div className="card">
+      <h5 className="card-title">{title}</h5>
+      <a href={deployedLink} target="_blank" rel="noreferrer">
+        <img src={imgSrc} className="card-img-top" alt={imgAlt} />
+      </a>
+      <div className="card-body">
+        <p className="card-text">{description}</p>
+        <a href={deployedLink} target="_blank" rel="noreferrer" className="btn btn-deployed">
+          Deployed App
+        </a>
+        <a href={githubLink} target="_blank" rel="noreferrer" className="btn btn-repo">
+          GitHub Repo
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Project;
