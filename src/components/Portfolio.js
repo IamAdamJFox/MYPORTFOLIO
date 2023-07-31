6a1af2a2fc8d1395bda9e6b9f795ec1ef1712c1a
adamjfox <adamjeffersonfox@gmail.com>
2023-07-31 03:40:53 -0700

    added Portfolio

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import Project from "./Project";
-// import projectsData from "../data/projectsData";
+import Blog from "../images/Blog.png"
+import Note from "../images/Note.png"
+import Quiz from "../images/Quiz.png"
+import Weather from "../images/Weather.png"
+import Quote from "../images/Quote.png"
+import Trade from "../images/Trade.png"
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../Style/Portfolio.css";
 
@@ -11,11 +16,62 @@ const Portfolio = () => {
       <h1 className="portfolio-title">Portfolio</h1>
       <Container>
         <Row>
-          {/* {projectsData.map((project, index) => (
-            <Col key={index} xs={12} md={6} lg={4} className="my-2">
-              <Project project={project} />
-            </Col>
-          ))} */}
+        {[
+            {
+              title: "Quote of Thrones",
+              description: "Group Project made to give you daily Game of Thrones quotes",
+              imgSrc: Quote,
+              deployedLink:
+                "https://thecolorhieu.github.io/Quotes-of-Thrones/",
+              githubLink:
+                "https://github.com/TheColorHieu/Quotes-of-Thrones",
+            },
+            {
+              title: "Trade Hub",
+              description: "In Development Commerce site where you can post/buy items from your local area",
+              imgSrc: Trade,
+              deployedLink:
+                "https://tradehub.herokuapp.com/",
+              githubLink:
+                "https://github.com/jonathanjjolsen/TradeHub",
+            },
+            {
+              title: "Adam J Fox Tech Blog",
+              description: "CMS-Style Blog",
+              imgSrc: Blog,
+              deployedLink:
+                "https://ajftechblog-7dca103e82cb.herokuapp.com/",
+              githubLink:
+                "https://github.com/IamAdamJFox/14-my-tech-blog",
+            },
+            {
+              title: "Note Taker",
+              description: "Built with Express.js",
+              imgSrc: Note,
+              deployedLink:
+                "https://module11notetakeradamf-25ccb2d6a7e6.herokuapp.com/",
+              githubLink:
+                "https://github.com/IamAdamJFox/11-note-taker",
+            },
+            {
+              title: "Let Learn Some Java",
+              description: "Simple API built with html and css",
+              imgSrc: Quiz,
+              deployedLink:
+                "https://iamadamjfox.github.io/4th-challenge/",
+              githubLink:
+                "https://github.com/IamAdamJFox/4th-challenge",
+            },
+            {
+              title: "Weekly Weather Report",
+              description: "Server side api for looking up local weather",
+              imgSrc: Weather,
+              deployedLink:
+                "https://iamadamjfox.github.io/6th-weather-app/",
+              githubLink:
+                "https://github.com/IamAdamJFox/6th-weather-app",
+            }
+            ]}
         </Row>
       </Container>
     </div>
