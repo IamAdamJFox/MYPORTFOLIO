a083bf05b1ea53836270563e6670cd8f70e50e7d
adamjfox <adamjeffersonfox@gmail.com>
2023-08-16 09:04:58 -0700

    updated about me

--- src/components/About.js
+++ src/components/About.js
@@ -29,13 +29,12 @@ function About() {
                 Adam J Fox, is a 28 year old from Scottsdale, Arizona. Having developed himself through a multitude of fields including the study of law, the study of the arts,
                  and as a engineer in the Arizona National Guard, Adam has most recently graduated from Arizona State University Full Stack Program in 2023. 
                  He has always had a interest in technology and has such decided to pursue a career in web development. Now that he has found a field that challenges him 
-                 intellectually and excites his creative passion, Adam strives to continue to develop his progamming skills.
+                 intellectually and excites his creative passion, Adam strives to develop his progamming skills.
                 </Card.Text>
                 <Card.Text>
-                Adam has developed himself on a few simple truths: do what is needed, you don't have forever, and find your happiness. The effort to push himself to develop
+                Adam has lived by a few simple truths: do what is necessary, you don't have forever, and continue to pursue happiness. The effort to push himself to develop
                  as a individual has made him into a asset for any team or origanization. Adam has discovered several needs: hes wants more from life, more from himself, 
-                 and wants to do the work to achieve all this and more. He is excited for what the future holds, as he continues to explore the horizon of oppurtunities 
-                 that continue to appear before him. 
+                 and to continue to discorver new challenges. He is excited for what the future holds, as he continues to explore the horizon of oppurtunities. 
                 </Card.Text>
               </Card.Body>
             </Card>
